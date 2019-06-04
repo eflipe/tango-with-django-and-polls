67b47fb5050c3c5f15d7dedadda39bca3252325f
eflipe <felipecabaleiro@gmail.com>
2019-06-04 18:39:16 -0300

    Static y Template files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def index(request):
-    return HttpResponse("Hola manes")
-    
+
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #template = 'rango/index.html'
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Este es el about. Volver al <a href='/rango/'>Index</a>")
